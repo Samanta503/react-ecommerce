6fe0c358e3f519bded2873942eef68eade43745e
Samanta Islam <samsamanta357@gmail.com>
2025-09-01 03:14:52 +0600

    Add Navbar

---
+++ react-ecommerce/src/Components/Shared/Navbar/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Logo from '../../../images/logo.png';
+
+const Navbar = () => {
+  return (
+  <nav className="navbar navbar-expand-lg ">
+  <div className="container">
+    <a className="navbar-brand" href="/">
+      <img src={Logo} alt="Logo" width={50}/> Mern E-commerce
+  </a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" 
+    aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse text-center" id="navbarNavAltMarkup">
+      <div className="navbar-nav">
+        <a className="nav-link active" aria-current="page" href="/">Home</a>
+        <a className="nav-link" href="/">Features</a>
+        <a className="nav-link" href="/">Pricing</a>  
+      </div>
+    </div>
+  </div>
+</nav>
+  )
+}
+
+export default Navbar
